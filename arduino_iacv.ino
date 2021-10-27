bc88eb45faeeeaaf13ccb7bc28c0134736cfb4db
Karl Withers <65951397+karlwith@users.noreply.github.com>
2021-10-28 09:41:15 +1000

    Update arduino_iacv.ino

--- arduino_iacv.ino
+++ arduino_iacv.ino
@@ -1,10 +1,10 @@
-int piezoPin = 12; //  piezo speaker pin
-int relayPin = 2;  //  relay for solenoid pin
-int thermistorPin = A5; // thermistor pin
-int Vo_1, Vo_2, Vo_3;  // signal readings
-int Vo; // filtered signal
-int warm = 370; // high bound of Vo for warm engine
-int overheating = 150; // high bound of Vo for overheating engine
+int piezoPin = 12;                        //  piezo speaker pin
+int relayPin = 2;                         //  relay for solenoid pin
+int thermistorPin = A5;                   // thermistor pin
+int Vo_1, Vo_2, Vo_3;                     // signal readings
+int Vo;                                   // filtered signal
+int warm = 370;                           // high bound of Vo for warm engine
+int overheating = 150;                    // high bound of Vo for overheating engine
 bool jobDone = false;
 
 void setup() {
@@ -12,8 +12,7 @@ void setup() {
   pinMode(piezoPin, OUTPUT);
   pinMode(thermistorPin, INPUT);
   digitalWrite(relayPin, HIGH);
-  // startup feedback
-  tone(piezoPin, 440, 125); delay(125);
+  tone(piezoPin, 440, 125); delay(125);   // startup feedback
   tone(piezoPin, 493, 125); delay(125);
   tone(piezoPin, 659, 250); delay(250);
 }
@@ -27,8 +26,7 @@ void loop() {
   Vo_2 = analogRead(thermistorPin);
   delay(1500);
   Vo_3 = analogRead(thermistorPin);
-  // average 3 readings to filter noise
-  Vo = (Vo_1 + Vo_2 + Vo_3) / 3; 
+  Vo = (Vo_1 + Vo_2 + Vo_3) / 3;            // average 3 readings to filter noise
   if (Vo < overheating) {
     overheatingAlert();
   }
@@ -38,8 +36,7 @@ void loop() {
 }
 
 void closeValve() {
-  // melody to alert driver of impending closure
-  tone(piezoPin, 659, 125); delay(125);
+  tone(piezoPin, 659, 125); delay(125);     // melody to alert driver of impending valve closure
   tone(piezoPin, 493, 125); delay(125);
   tone(piezoPin, 440, 2000); delay(2000);
   digitalWrite(relayPin, LOW);
@@ -47,7 +44,6 @@ void closeValve() {
 }
 
 void overheatingAlert() {
-  // annoying noise to alert driver engine is overheating
-  tone(piezoPin, 440, 20000);
+  tone(piezoPin, 440, 20000);               // aleting noise to alert driver engine is overheating
   jobDone = true;
 }
